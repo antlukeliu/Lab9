885235ce33428184e745558cec635ad5e9034ff3
AntLukeLiu <antmliu@gmail.com>
2016-11-01 16:23:13 -0400

    Updated code

--- src/CircleApp.java
+++ src/CircleApp.java
@@ -5,6 +5,9 @@ public class CircleApp {
 	public static void main(String[] args) {
 		
 		boolean cont = true;
+		int count = 0;
+		
+		System.out.println("Welcome to the Circle Tester");
 		do{
 			Validator valid = new Validator();
 		
@@ -15,45 +18,18 @@ public class CircleApp {
 			System.out.println("Circumference: "+circle.getFormattedCircumference());
 			System.out.println("Area: "+circle.getFormattedArea());
 			
-			char response = checkingForYesOrNo();
-			cont = translatingAgain(response);
-			exitting(cont);
+			count ++;
+			char response = valid.checkingForYesOrNo();
+			cont = valid.translatingAgain(response);
+			exitting(cont, count);
 			
 		}while(cont);
 	}
 	
-	
-	
-	public static char checkingForYesOrNo(){
-		Scanner sc = new Scanner(System.in);
-		
-		String response = "";
-		boolean cont = true;
-		while(cont){
-			System.out.print("\nContinue to calculate? (y/n): ");
-			response = sc.nextLine();
-			if (response.toLowerCase().charAt(0) != 'y' && response.toLowerCase().charAt(0) != 'n'){
-				continue;
-				}
-			else{
-				break;
-			}
-		}
-		return response.toLowerCase().charAt(0);
-	}
-	//Breaking while loop if false 
-	public static boolean translatingAgain(char response){
-		if (response == 'y'){
-			return true;
-		} else{
-			return false;
-		}
-	}
-	
 	//Closing message
-	public static void exitting(boolean cont){
+	public static void exitting(boolean cont, int i){
 		if (cont == false){
-			System.out.println(	"\nHave an awesome day!");
+			System.out.printf("\nGoodbye! You made %d circles.", i);
 		}
 	}
 }
